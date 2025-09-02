261a0d35225ae1749b18240d86666528a6ed2110
ktanikun <ktanikunph@gmail.com>
2025-09-02 19:17:56 +0900

    Deployed 497571c with MkDocs version: 1.6.1

---
+++ js/extra.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\\[','\\]']],
+    processEscapes: true
+  },
+  "HTML-CSS": {
+    availableFonts: [],
+    preferredFont: null,
+    webFont: "STIX-Web"
+  }
+};
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
